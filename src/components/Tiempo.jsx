14c2958c41472e587da38ed341f97a22a95708c3
Adi-Haviv <adi.haviv@gmail.com>
2018-08-26 23:17:41 +0300

    added bot connectivity to server side

--- src/components/Tiempo.jsx
+++ src/components/Tiempo.jsx
@@ -1,14 +1,100 @@
 import React, { Component } from 'react'
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
-import ChatBot from 'react-simple-chatbot';
 import {ThemeProvider} from 'styled-components'
-
+import PropTypes from 'prop-types';
+import ChatBot, { Loading } from 'react-simple-chatbot';
 
 import './Tiempo.css'
 
-class Tiempo extends Component 
-{
+class UserQuery extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        loading: true,
+        result: '',
+        trigger: false,
+      };
+  
+      this.triggetNext = this.triggetNext.bind(this);
+    }
+  
+    componentWillMount() {
+      const self = this;
+      const { steps } = this.props;
+      const message = steps.input.value;
+       
+      const queryUrl = `http://localhost:8080/tiempo?query=${message}`;
+  
+      const xhr = new XMLHttpRequest();
+  
+      xhr.addEventListener('readystatechange', readyStateChange);
+  
+      function readyStateChange() {
+        if (this.readyState === 4) {
+          const data = JSON.parse(this.responseText);
+          console.log(data)
+          const bindings = data.query;
+          console.log(bindings)
+          if (bindings && bindings.length > 0) {
+            self.setState({ loading: false, result: bindings });
+          } else {
+            self.setState({ loading: false, result: 'Not found.' });
+          }
+        }
+      }
+  
+      xhr.open('GET', queryUrl);
+      xhr.send();
+    }
+  
+    triggetNext() {
+      this.setState({ trigger: true }, () => {
+        this.props.triggerNextStep();
+      });
+    }
+  
+    render() {
+      const { trigger, loading, result } = this.state;
+  
+      return (
+        <div className="userquery">
+          { loading ? <Loading /> : result }
+          {
+            !loading &&
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: 20,
+              }}
+            >
+              {
+                !trigger &&
+                <button
+                  onClick={() => this.triggetNext()}
+                >
+                  Ok
+                </button>
+              }
+            </div>
+          }
+        </div>
+      );
+    }
+}
+  
+UserQuery.propTypes = {
+    steps: PropTypes.object,
+    triggerNextStep: PropTypes.func,
+};
+ 
+UserQuery.defaultProps = {
+    steps: undefined,
+    triggerNextStep: undefined,
+};
+
+class Tiempo extends Component {
     constructor(props){
         super(props)
         this.state = { }
@@ -45,21 +131,25 @@ class Tiempo extends Component
                 // opened={props.opened}
                 steps={[
                     {
-                        id: '1',
-                        message: 'What is your name?',
-                        trigger: '2',
+                        id: 'greet',
+                        message: 'Hi, I\'m Tiempo. How can I help you?',
+                        trigger: 'input',
                     },
                     {
-                        id: '2',
+                        id: 'input',
                         user: true,
-                        trigger: '3',
+                        trigger: 'query',
                     },
                     {
-                        id: '3',
-                        message: 'Hi {previousValue}, nice to meet you!',
-                        //message: 'Who is {previousValue}?',
-                        end: true,
-                        
+                        id: 'query',
+                        component: <UserQuery />,
+                        waitAction: true,
+                        trigger: 'finish',
+                    },
+                    {
+                        id:'finish',
+                        message: 'Can I help you with something else?',
+                        trigger: 'input',
                     },
                     ]}
             />
